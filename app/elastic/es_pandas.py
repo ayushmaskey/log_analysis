634923bfb1370f9e1728e13aae34715a0a5a1244
amaskey <maskey.maskey@gmail.com>
2018-09-16 06:11:01 +0000

    auto commit

--- app/elastic/es_pandas.py
+++ app/elastic/es_pandas.py
@@ -1,6 +1,7 @@
 
 from es_connection import es_connect
 from es_request_total_json import json_query
+from es_request_protocol_json import json_protocol_query
 import pandas as pd
 from es_to_csv import append_to_csv
 import iso8601
@@ -9,9 +10,15 @@ import datetime as dt
 
 def get_agg_response_list_of_dict(ind, start, end ):
 	
+	all_index = "*"
+
 	es = es_connect()
-	q = json_query(ind, start , end)
-	es_response = es.search(index=ind, body=q)
+	if ind == "*":
+		q = json_query(ind, start , end)
+	else:
+		q = json_protocol_query(ind, start , end)
+
+	es_response = es.search(index=all_index, body=q)
 	
 	agg_list = es_response['aggregations']['total_traffic']['buckets']
 	
@@ -41,12 +48,12 @@ def es_traffic_pandas_csv(fileName, ind, start, end):
 if __name__ == "__main__":
 
 
-	ind = "*"
+	ind = "bro_dns"
 	start = "now-1d"
 	end = "now"
 
 
-	fileName = 'test'
+	fileName = 'bro_dns'
 
 	
 	es_traffic_pandas_csv(fileName, ind, start, end)
--- app/elastic/load_one_time.py
+++ app/elastic/load_one_time.py
@@ -1,23 +1,26 @@
 from es_pandas import es_traffic_pandas_csv
 
 
-def push_total_to_csv(ind):
+def push_to_csv(fileName, index):
 	for i in range(1,17):
 		start = "now-"+str(i)+"d"
 		end = "now"
 
-		fileName, index = ind
 		es_traffic_pandas_csv(fileName, index, start, end)
 
 if __name__ == "__main__":
 	ind = {
-			"total": ["total","*"], 
-			"dhcp": "event_type:bro_dhcp",
-			"dns": "event_type:bro_dns",
-			"conn": "event_type:bro_conn",
-			"snmp": "event_type:bro_snmp",
-			"weird": "event_type:bro_weird",
-			"http": "event_type:bro_http",
-			"files": "event_type:bro_files",
+			"total": "*", 
+			"dhcp": "bro_dhcp",
+			"dns": "bro_dns",
+			"conn": "bro_conn",
+			"snmp": "bro_snmp",
+			"weird": "bro_weird",
+			"http": "bro_http",
+			"files": "bro_files",
 		}
-	push_total_to_csv(ind['total'])
+
+
+	for key, value in ind.items():
+
+		push_to_csv(key, value)
